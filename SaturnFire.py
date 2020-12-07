ae04cfbd30eb25243234d4fab61059234dc4cf6e
deanhutt85 <deanhutt85@googlemail.com>
2020-12-07 21:54:53 +0000

    Started GUI and moved everything to one file Port status added

--- SaturnFire.py
+++ SaturnFire.py
@@ -2,7 +2,10 @@ import serial
 import time
 import logging
 import serial.rs485
-from SaturnFireGUI import *
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
 
 START_OF_HEADER = bytes(0x01)
 START_OF_TEXT = bytes(0x02)
@@ -10,20 +13,62 @@ END_OF_TEXT = bytes(0x03)
 END_OF_TRANSMITION = bytes(0x04)
 
 
-serialport = '/dev/ttyUSB0'
+# serialport = '/dev/ttyUSB0'
+serialport = StringVar()
 serialbaudrate = 9600
 polltime = 200 # In ms
 spincount = 1
+firetimeout = 0
+portstatus = StringVar()
+portstatus.set("Closed")
 logfile = "HuxleyLogfile.log"
 
 logging.basicConfig(filename=logfile, filemode='w', level=logging.INFO)
 
 
+
 class FireWheel:
-    def __init__(self):
+    def __init__(self, root):
         self.last_time = int(round(time.time() * 1000))
-        self.ser = serial.rs485.RS485(port=serialport, baudrate=serialbaudrate, stopbits=serial.STOPBITS_ONE, parity = serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=1, rtscts=0)
-        self.ser.rs485_mode = serial.rs485.RS485Settings(False, True)
+        # self.ser = serial.rs485.RS485(port=serialport, baudrate=serialbaudrate, stopbits=serial.STOPBITS_ONE, parity = serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=1, rtscts=0)
+        # self.ser.rs485_mode = serial.rs485.RS485Settings(False, True)
+
+        print("Launching GUI")
+        root.title("Serial Tests")
+        self.port = Entry(root, textvariable=serialport)
+        self.port.grid(row=0, column=1)
+        self.seriallabel = Label(root, text="Serial Port: ")
+        self.seriallabel.grid(row=0, column=0)
+        self.timeoutlabel = Label(root, text="Fire Timeout: ")
+        self.timeoutlabel.grid(row=1, column=0)
+        self.timeoutentry = Entry(root, textvariable=firetimeout)
+        self.timeoutentry.grid(row=1, column=1)
+        self.openbutton = Button(root, text="Start", command=lambda: self.open_serial())
+        self.openbutton.grid(row=6, column=0)
+        self.closebutton = Button(root, text="Close", command=lambda: self.close_serial())
+        self.closebutton.grid(row=6, column=1)
+        self.opencclose = Label(root, textvariable=portstatus)
+        self.opencclose.grid(row=0, column=2)
+
+    def open_serial(self):
+        print("Opening Serial Port")
+        try:
+            self.ser = serial.rs485.RS485(port=serialport.get(), baudrate=serialbaudrate, stopbits=serial.STOPBITS_ONE, parity = serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=1, rtscts=0)
+            self.ser.rs485_mode = serial.rs485.RS485Settings(False, True)
+            print("Serial Port Opened on port " + str(serialport.get()))
+            portstatus.set("Opened")
+        except serial.SerialException as e:
+            print("Something Went Wrong! \n" + str(e))
+            self.message_box(e)
+
+
+
+    def close_serial(self):
+        print("Closing Serial Port")
+        self.ser.close()
+        portstatus.set("Closed")
+        print("Serial Port Closed")
+
 
     def send_command(self):
         print("Starting")
@@ -46,6 +91,7 @@ class FireWheel:
                         self.last_time = int(round(time.time() * 1000))
             except serial.SerialException as e:
                 print("Something bad happened" + str(e))
+                self.message_box(e)
 
 
     def readData(self):
@@ -67,9 +113,12 @@ class FireWheel:
         # checksum = hex(sum(sr2hexcommand) & 0x3f | 0x40)
         # print(checksum)
 
+    def message_box(self, openerror):
+        messagebox.showerror("ERROR!", openerror)
 
-WheelFireGUI()
 
+app = FireWheel(root)
+root.mainloop()
 
 #startapp = FireWheel()
 #startapp.send_command()
